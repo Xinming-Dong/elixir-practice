17981535ea7e6a74f963ef15e6379738ad106d6d
miadong <dong.xinm@husky.neu.edu>
2019-09-20 12:45:40 -0400

    add factor function

---
+++ lib/practice/factor.ex
@@ -0,0 +1,18 @@
+defmodule Practice.Factor do
+
+    def factors(num) do
+        factorize(num, 2, [])
+    end
+
+    def factorize(number, factor, result) when rem(number, factor) == 0 do
+        [factor|factorize(div(number, factor), factor, result)]
+    end
+
+    def factorize(number, factor, result) when number < factor do
+        result
+    end
+
+    def factorize(number, factor, result) do
+        factorize(number, factor + 1, result)
+    end
+end
